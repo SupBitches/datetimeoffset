2bacb666ae1c73811ed3442e4b7583f103439fb0
Ben Whittle <ben.whittle@frfl.co.uk>
2024-07-17 16:20:21 +0100

    Add project files.

---
+++ datetimeoffset/Program.cs
@@ -0,0 +1,12 @@
+﻿DateTimeOffset dto;
+DateTime dt;
+
+dto = new DateTimeOffset(2024,1,1,12,00,00,00, TimeSpan.FromHours(5)) ;
+dt = dto.UtcDateTime;
+dto = (DateTimeOffset)dt;
+
+
+Console.WriteLine($"DateTimeOffset: {dto}");
+Console.WriteLine($"DateTime: {dt}");
+
+Console.ReadLine();
